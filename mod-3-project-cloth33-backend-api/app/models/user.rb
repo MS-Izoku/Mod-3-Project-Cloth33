417f77d9c4aa24e54e60978ace726c71fb377768
github username <github email address>
2019-10-14 15:22:02 -0500

    initial cleanup started

--- mod-3-project-cloth33-backend-api/app/models/user.rb
+++ mod-3-project-cloth33-backend-api/app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :outfits, dependent: :destroy
     has_many :items, dependent: :destroy
     validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
     validates_length_of :items, maximum: 32
 end
